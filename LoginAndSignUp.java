bbe6e44fb442e3b337cc523e7179d07ace6b9217
eyngelcondense <angelmaeramirezcortino@gmail.com>
2024-11-14 21:23:05 +0800

    Add files via upload

---
+++ LoginAndSignUp.java
@@ -0,0 +1,15 @@
+package UI;
+
+
+public class LoginAndSignUp {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
